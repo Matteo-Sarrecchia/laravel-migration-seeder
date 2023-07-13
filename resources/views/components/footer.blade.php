0de16aba9743b47cfc3e78fe7e0bf1e71bd150f7
Matteo Sarrecchia <morrea@hotmail.it>
2023-07-13 17:02:48 +0200

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="container-fluid text-center py-3 bg-dark text-light">
+    <h1>FOOTER</h1>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main-layout')
+
+@section('pageName')
+
+    home
+
+@endsection
+
+@section('content')
+
+    <main class="py-5">
+       <h1>hello world</h1>
+    </main>
+
+@endsection
